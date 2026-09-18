3340ea84224c1780ead77d2ecd9aeab4ca715af1
ハウスハウス <nikkukyuu-code@users.noreply.github.com>
2026-09-18 15:56:25 +0000

    Cache-bust game assets so floor/stair updates load
    Append version query to CSS and ES module imports.

--- game/js/game.js
+++ game/js/game.js
@@ -8,9 +8,9 @@ import {
   createBlueprint, createEmptyHouseData, isWalkable, isPlaceable,
   validateHouse, getSpawn, tileAt, drawHouse, drawPlayer, drawTrapSprite, drawChestSprite,
   floorLabel, COLORS, generateComHouse, parseHouse,
-} from './house.js';
-import { NetSession, loadPeerJS, isPeerAvailable, isValidRoomCode, normalizeRoomCode } from './net.js';
-import { $, showScreen, setStatus, heartsHtml, bindHold, bindTap, lockTouch, flashOverlay } from './ui.js';
+} from './house.js?v=20260919a';
+import { NetSession, loadPeerJS, isPeerAvailable, isValidRoomCode, normalizeRoomCode } from './net.js?v=20260919a';
+import { $, showScreen, setStatus, heartsHtml, bindHold, bindTap, lockTouch, flashOverlay } from './ui.js?v=20260919a';
 
 const blueprint = createBlueprint();
 
